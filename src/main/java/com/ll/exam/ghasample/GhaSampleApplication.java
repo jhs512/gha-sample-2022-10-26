c648d756183df5ae65381057a1e7d850e1f80d10
jhs512 <jangka512@gmail.com>
2022-10-26 09:19:42 +0900

    세팅

---
+++ src/main/java/com/ll/exam/ghasample/GhaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.ghasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhaSampleApplication.class, args);
+    }
+
+}
