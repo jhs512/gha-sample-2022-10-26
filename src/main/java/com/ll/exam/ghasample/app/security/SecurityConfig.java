2b2d2191a7260dc3c7ffe69a3d0906e2b9edc3b2
jhs512 <jangka512@gmail.com>
2022-10-27 22:51:01 +0900

    액츄에이터

---
+++ src/main/java/com/ll/exam/ghasample/app/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ll.exam.ghasample.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/actuator/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
