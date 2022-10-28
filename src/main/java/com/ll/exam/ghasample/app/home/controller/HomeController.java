8f40b646dacfc3d7118d226c7762b7ea0a267be4
jhs512 <jangka512@gmail.com>
2022-10-28 22:33:15 +0900

    배포테스트

--- src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ll.exam.ghasample.app.home.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final Environment env;
+
     @GetMapping("/")
     @ResponseBody
-    public String showMain(HttpServletRequest req) {
-        return "IP : " + req.getRemoteAddr();
+    public String showMain(HttpServletRequest req)
+    {
+        return "!IP : " + env.getProperty("hostname");
     }
 }
