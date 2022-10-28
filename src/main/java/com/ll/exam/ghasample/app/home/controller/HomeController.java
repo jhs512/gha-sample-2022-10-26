0b9423b0857ce743efdd2c6e064041d8524541b5
jhs512 <jangka512@gmail.com>
2022-10-28 17:21:32 +0900

    IP :

--- src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
@@ -11,6 +11,6 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain(HttpServletRequest req) {
-        return req.getRemoteAddr();
+        return "IP : " + req.getRemoteAddr();
     }
 }
