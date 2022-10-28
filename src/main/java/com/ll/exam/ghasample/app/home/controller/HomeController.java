0115a4aff56c18d6d7f5c339301063e32b665e35
jhs512 <jangka512@gmail.com>
2022-10-28 16:07:10 +0900

    work-1

---
+++ src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ll.exam.ghasample.app.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain(HttpServletRequest req) {
+        return req.getRemoteAddr();
+    }
+}
