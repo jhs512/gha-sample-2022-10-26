bad8726b848b81872eb95281ac6903dda13b8584
jhs512 <jangka512@gmail.com>
2022-10-31 09:28:14 +0900

    work-1.yml

---
+++ src/main/java/com/ll/exam/ghasample/app/home/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ll.exam.ghasample.app.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final Environment environment;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return environment.getProperty("HOSTNAME");
+    }
+}
